d60d108296a09758630bcfde35b1be234df7fd2d
John Pruitt <jgpruitt@gmail.com>
2018-04-07 11:09:24 -0500

    it doesn't make much sense to have more than one instance of Shutdown per process. might as well be package level functions

--- shutdown.go
+++ shutdown.go
@@ -1,38 +1,62 @@
+/*
+MIT License
+
+Copyright (c) 2018 John Pruitt
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package shutdown
 
 import (
 	"os"
-	"sync"
 	"os/signal"
+	"sync"
 )
 
 type Task func()
 
-type Shutdown struct {
-	once sync.Once
-	mu sync.Mutex
+var (
+	once  sync.Once
+	mu    sync.Mutex
 	tasks []Task
+)
+
+func Add(t Task) {
+	mu.Lock()
+	defer mu.Unlock()
+	tasks = append(tasks, t)
 }
 
-func (sd *Shutdown) Add(t Task) {
-	sd.mu.Lock()
-	defer sd.mu.Unlock()
-	sd.tasks = append(sd.tasks, t)
-}
-
-func (sd *Shutdown) OnSignal() {
+func OnSignal() {
 	var c = make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
 	<-c
-	sd.Now(0)
+	Now(0)
 }
 
-func (sd *Shutdown) Now(code int) {
-	sd.once.Do(func() {
-		sd.mu.Lock()
-		defer sd.mu.Unlock()
-		for i := len(sd.tasks) - 1; i >= 0; i-- {
-			sd.tasks[i]()
+func Now(code int) {
+	once.Do(func() {
+		mu.Lock()
+		defer mu.Unlock()
+		for i := len(tasks) - 1; i >= 0; i-- {
+			tasks[i]()
 		}
 		os.Exit(code)
 	})
